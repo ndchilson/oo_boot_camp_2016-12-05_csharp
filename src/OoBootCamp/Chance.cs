d5a5a5237608cab4324499ee99285a771585494c
Fred George <fredgeorge@acm.org>
2016-12-05 15:06:26 -0800

    Fred: Chance with equality and hash code

---
+++ src/OoBootCamp/Chance.cs
@@ -0,0 +1,35 @@
+﻿/* 
+ * Copyright (c) 2016 by Fred George
+ * May be used freely except for training; license required for training.
+ */
+using System;
+
+namespace OoBootCamp
+{
+    // Understands the likelihood of something occuring
+    public class Chance
+    {
+        private readonly double _fraction;
+
+        public Chance(double likelihoodAsFraction)
+        {
+            _fraction = likelihoodAsFraction;
+        }
+
+        public override bool Equals(object other)
+        {
+            if (!(other is Chance)) return false;
+            return this.Equals(other as Chance);
+        }
+
+        private bool Equals(Chance other)
+        {
+            return this._fraction == other._fraction;
+        }
+
+        public override int GetHashCode()
+        {
+            return _fraction.GetHashCode();
+        }
+    }
+}
---
+++ test/OoBootCamp.Tests/ChanceTest.cs
@@ -0,0 +1,31 @@
+﻿/* 
+ * Copyright (c) 2016 by Fred George
+ * May be used freely except for training; license required for training.
+ */
+
+using System.Linq.Expressions;
+using NUnit.Framework;
+
+namespace OoBootCamp.Tests
+{
+    // Ensures Chance operates correctly
+    [TestFixture]
+    public class ChanceTest
+    {
+
+        [Test]
+        public void Equality()
+        {
+            Assert.AreEqual(new Chance(0.25), new Chance(0.25));
+            Assert.AreNotEqual(new Chance(0.25), new object());
+            Assert.AreNotEqual(new Chance(0.25), null);
+        }
+         
+        [Test]
+        public void Hash()
+        {
+            Assert.AreEqual(new Chance(0.25).GetHashCode(), new Chance(0.25).GetHashCode());
+        }
+
+    }
+}
