13c992a57727e03829dc84509b781bad9d20bb8f
Fred George <fredgeorge@acm.org>
2016-12-07 06:42:54 -0800

    Fred: Quantity with Equal() for like Units

---
+++ src/OoBootCamp/Quantities/Quantity.cs
@@ -0,0 +1,47 @@
+﻿/* 
+ * Copyright (c) 2016 by Fred George
+ * May be used freely except for training; license required for training.
+ */
+using System;
+
+namespace OoBootCamp.Quantities
+{
+    public class Quantity
+    {
+        public static readonly object Teaspoon = new object();
+        public static readonly object Tablespoon = new object();
+        public static readonly object Ounce = new object();
+        public static readonly object Cup = new object();
+        public static readonly object Pint = new object();
+        public static readonly object Quart = new object();
+        public static readonly object Gallon = new object();
+
+        private readonly double _amount;
+        private readonly object _unit;
+
+        private const double Tolerance = 0.000001;
+
+        public Quantity(double amount, object unit)
+        {
+            _amount = amount;
+            _unit = unit;
+        }
+
+        public override bool Equals(object other)
+        {
+            if (!(other is Quantity)) return false;
+            return this.Equals(other as Quantity);
+        }
+
+        private bool Equals(Quantity other)
+        {
+            return this._unit == other._unit && 
+                Math.Abs(this._amount - other._amount) < Tolerance;
+        }
+
+        public override int GetHashCode()
+        {
+            return _unit.GetHashCode() * 17 + _amount.GetHashCode();
+        }
+    }
+}
---
+++ test/OoBootCamp.Tests/Quantities/QuantityTest.cs
@@ -0,0 +1,37 @@
+﻿/* 
+ * Copyright (c) 2016 by Fred George
+ * May be used freely except for training; license required for training.
+ */
+
+using NUnit.Framework;
+using OoBootCamp.Quantities;
+using static OoBootCamp.Quantities.Quantity;
+
+namespace OoBootCamp.Tests.Quantities
+{
+    // Ensures Quantity operates correctly
+    [TestFixture]
+    public class QuantityTest
+    {
+        [Test]
+        public void EqualityOfSameUnit()
+        {
+            Assert.AreEqual(new Quantity(2, Tablespoon), new Quantity(2, Tablespoon));
+            Assert.AreNotEqual(new Quantity(2, Tablespoon), new Quantity(4, Tablespoon));
+            Assert.AreNotEqual(new Quantity(2, Tablespoon), new object());
+            Assert.AreNotEqual(new Quantity(2, Tablespoon), null);
+        }
+
+        [Test]
+        public void EqualityOfDifferentUnit()
+        {
+            Assert.AreNotEqual(new Quantity(4, Cup), new Quantity(4, Gallon));
+        }
+
+        [Test]
+        public void Hash()
+        {
+            Assert.AreEqual(new Quantity(2, Tablespoon).GetHashCode(), new Quantity(2, Tablespoon).GetHashCode());
+        }
+    }
+}
